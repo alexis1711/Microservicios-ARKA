543ccb83879545a6e738cc2910b4fc4099d8bdec
Alexis Cornejo <53987686+alexis1711@users.noreply.github.com>
2025-10-18 02:15:25 -0600

    Se cambian conexiones a BD AWS

--- providers/src/main/java/com/arka/providers/entity/Provider.java
+++ providers/src/main/java/com/arka/providers/entity/Provider.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
-@Table("providers")
+@Table("provider")
 @Getter
 @Setter
 @Builder
